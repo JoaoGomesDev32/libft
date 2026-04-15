6582f848ab8944d4d0d1418f83b4ba43c022cfb4
Joao Felipe Gomes Da Silva <joagomes@c1r5s6.42lisboa.com>
2026-04-15 20:03:19 +0100

    funcao ft_bzero.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: joagomes <joagomes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 13:57:56 by joagomes          #+#    #+#             */
-/*   Updated: 2026/04/15 18:57:36 by joagomes         ###   ########.fr       */
+/*   Updated: 2026/04/15 19:52:10 by joagomes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ int	ft_isalnum(int c);
 int ft_isascii(int c);
 int ft_isprint(int c);
 void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
 
 #endif /* LIBFT_H */
