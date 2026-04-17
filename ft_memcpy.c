1ef253c003d77544fb1b1cbad0c923a693faf013
Joao Felipe Gomes Da Silva <joagomes@c1r5s8.42lisboa.com>
2026-04-17 11:49:44 +0100

    funcao ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joagomes <joagomes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/17 11:14:08 by joagomes          #+#    #+#             */
+/*   Updated: 2026/04/17 11:47:39 by joagomes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+	Recebo um endereco de memoria (src)
+	Para cada byte ate n:
+		substituir cada byte de dest pelos bytes em src
+	Retornar o ponteiro dest
+*/
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
+/*
+int	main(void)
+{
+	char	str[10] = "0123456789";
+	char	result[10] = {0};
+	ft_memcpy(result, str, 9);
+	for(int i = 0; i < 10; i++)
+	{
+		printf("%c ", result[i]);
+	}
+	return (0);
+}
+*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: joagomes <joagomes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 13:57:56 by joagomes          #+#    #+#             */
-/*   Updated: 2026/04/15 19:52:10 by joagomes         ###   ########.fr       */
+/*   Updated: 2026/04/17 11:14:39 by joagomes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ int ft_isascii(int c);
 int ft_isprint(int c);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif /* LIBFT_H */
