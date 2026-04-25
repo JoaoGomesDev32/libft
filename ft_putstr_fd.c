894d5445a0545683c18d9214cda99bec3c11ede8
João Gomes <joaogomesdev32@gmail.com>
2026-04-25 09:55:04 +0100

    function ft_putstr_fd.c

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,11 +6,23 @@
 /*   By: joaog <joaog@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 09:37:07 by joaog             #+#    #+#             */
-/*   Updated: 2026/04/25 09:39:41 by joaog            ###   ########.fr       */
+/*   Updated: 2026/04/25 09:52:36 by joaog            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_putstr_fd(char *s, int fd)
-{}
+{
+	int	len_s;
+
+	len_s = ft_strlen(s);
+	write(fd, s, len_s);
+}
+/*
+int main(void)
+{
+	ft_putstr_fd("Felipe\n", 1);
+	return (0);
+}
+*/
